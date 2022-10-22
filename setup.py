75d98bc9ef038d54a153ef03dbe597e592d70107
Ademola Kunmi <salamiolokun@gmail.com>
2022-10-22 01:42:49 +0100

    create folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of customer behavior on purchase of insurance for a travel insurance company.',
+    author='Ademola Olokun',
+    license='MIT',
+)
